3e4c5f8d34a114c5e3f8b9d8dc5d15c08a604128
Hazel Lee <hyewon1157@gmail.com>
2022-11-23 17:04:23 -0800

    chpt7-1

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
-numbers = list(map(int, input().split()))
+
 
 # ******************************
-# Make your Code
+numbers = list(map(int, input().split()))
+print (numbers)
+
+total = sum(numbers)
+avg = total / len(numbers)
+
+for i in range(len(numbers)):
+	dist = abs(numbers[i] - avg)
+	print (f'{dist:.2f}', end=' ')
+
 # ******************************
 
 
